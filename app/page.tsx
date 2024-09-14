8fa6e0b4bc43d0bee748c3a151b309e57cf3de5e
Nader Abdulrub <nabdulrub16@gmail.com>
2024-09-13 23:54:22 -0400

    fix: updated missing imports.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Index() {
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        <SignUpUserSteps />
-      </main>
-    </>
+    <Link href={"/sign-in"}>
+      <Button>Sign In</Button>
+    </Link>
   );
 }
