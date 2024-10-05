698b4bfc41ca4487644b27764de87e932c9556b5
Nader Abdulrub <nabdulrub16@gmail.com>
2024-10-04 23:24:45 -0400

    fix: adding suspense fallback.

--- app/(shared-lists)/shared/ordered/page.tsx
+++ app/(shared-lists)/shared/ordered/page.tsx
@@ -1,10 +1,15 @@
+import NoData from "@/components/ui/no-data";
 import OrderSuccessPage from "@/webpages/orders/OrderSuccessPage";
-import React from "react";
+import React, { Suspense } from "react";
 
 type Props = {};
 
 const Ordered = (props: Props) => {
-  return <OrderSuccessPage />;
+  return (
+    <Suspense fallback={<NoData variant="loading" message="Loading..." />}>
+      <OrderSuccessPage />
+    </Suspense>
+  );
 };
 
 export default Ordered;
